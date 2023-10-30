c196afaa06133a257d7b900af80c7b0b9e1f1069
Griffin Scott <griffinoscott@gmail.com>
2023-10-29 19:30:06 -0700

    finished todo list, need to make it persist on refresh

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,23 +1,104 @@
 // import './HomePage.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function Todo() {
+    const [todos, setTodos] = useState([]);
+    const [value, setValue] = useState('');
+    const [completed, setCompleted] = useState(false);
 
-    const [todos, setTodos] = useState({})
+    const initialTodos = [
+        {
+            id: 0,
+            task: "Set up API to feed data, postman is set up on laptop",
+            completed: false,
+        },
+        {
+            id: 1,
+            task: "Component structure for Albums, Artists, Songs",
+            completed: false,
+        },
+        {
+            id: 2,
+            task: "Create React components for pages + cards",
+            completed: false,
+        },
+    ];
 
+    const handleChange = (event) => {
+        setValue(event.target.value)
+        console.log(value)
+    }
 
+    const addTodo = (event) => {
+        event.preventDefault()
+        setTodos([...todos,
+            {
+                id: todos.length,
+                task: value,
+                completed: false,
+            }
+        ])
+        setValue('')
+    }
+
+    const completeTodo = (event) => {
+        const id = event.target.id
+        const arr = todos
+        todos[id].completed = true;
+        setTodos(arr)
+        setCompleted(true)
+    }
+
+    const deleteTodo = (event) => {
+        const id = event.target.id
+        const arr = todos
+        arr.splice(id, 1)
+        for(let i = 0; i < arr.length; i++){
+            arr[i].id = i
+        }
+        setCompleted(true)
+        setTodos(arr)
+    }
+
+    useEffect(() => {
+        //maybe set this up to feed from database
+        setTodos(initialTodos);
+    }, []);
+
+    useEffect(() => {
+        if(completed){
+            setCompleted(false)
+        }
+    }, [completed]);
 
     return (
         <div className="Todo">
-            <div className="col-6 mx-auto">
-                <h2 className="display-3 mb-1">Todo List</h2>
-                <p className="lead">
-                    -Set up API to feed data, postman is set up on laptop<br />
-                    -Component structure for Albums, Artists, Songs<br />
-                    -Create React components for pages + cards
-                </p>
-
-
+            <div className="col-4 mx-auto">
+                <h2 className="display-3 fw-bold mb-3 text-center">Todo List</h2>
+                {/* <h3 className="lead fs-1 mb-0 pb-0">Add Todo</h3> */}
+                <form className="row ms-0 mb-4" onSubmit={addTodo}>
+                    <div className="col-11">
+                        <label for="todo-input" class="form-label fs-2">Add Todo</label>
+                        <input value={value} onChange={handleChange} type="text" class="form-control" id="todo-input" />
+                    </div>
+                    <div className="col-1 d-flex align-items-end">
+                        <button onClick={addTodo} className="btn btn-outline-light mt-2" type="button">
+                            Add
+                        </button>
+                    </div>
+                </form>
+                <div className="container">
+                    {todos.map((todo) => (
+                        <div key={todo.id} className="row my-2">
+                            <div className="col-11">
+                                <p style={todo.completed ? {textDecoration: "line-through"} : {}} key={todo.task} className="lead fs-4">-{todo.task}</p>
+                            </div>
+                            <div className="col-1">
+                                {todo.completed ? <button id={todo.id} onClick={deleteTodo} className="btn btn-outline-danger">Delete</button> : <button id={todo.id} onClick={completeTodo} className="btn btn-outline-success">Done</button>}
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
