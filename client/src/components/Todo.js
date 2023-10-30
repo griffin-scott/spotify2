2beaa7963ed23a6073093efeb4aff62a1a9aff36
Griffin Scott <griffinoscott@gmail.com>
2023-10-29 20:46:07 -0700

    todo list persists with localstorage

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 
 function Todo() {
     const [todos, setTodos] = useState([]);
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState("");
     const [completed, setCompleted] = useState(false);
 
     const initialTodos = [
@@ -25,49 +25,59 @@ function Todo() {
     ];
 
     const handleChange = (event) => {
-        setValue(event.target.value)
-        console.log(value)
-    }
+        setValue(event.target.value);
+        console.log(value);
+    };
 
     const addTodo = (event) => {
-        event.preventDefault()
-        setTodos([...todos,
+        event.preventDefault();
+        let newTodos = [
+            ...todos,
             {
                 id: todos.length,
                 task: value,
                 completed: false,
-            }
-        ])
-        setValue('')
-    }
+            },
+        ]
+        setTodos(newTodos);
+        localStorage.setItem("todos", JSON.stringify(newTodos));
+        setValue("");
+    };
 
     const completeTodo = (event) => {
-        const id = event.target.id
-        const arr = todos
+        const id = event.target.id;
+        const arr = todos;
         todos[id].completed = true;
-        setTodos(arr)
-        setCompleted(true)
-    }
+        setTodos(arr);
+        localStorage.setItem("todos", JSON.stringify(todos));
+        setCompleted(true);
+    };
 
     const deleteTodo = (event) => {
-        const id = event.target.id
-        const arr = todos
-        arr.splice(id, 1)
-        for(let i = 0; i < arr.length; i++){
-            arr[i].id = i
+        const id = event.target.id;
+        const arr = todos;
+        arr.splice(id, 1);
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].id = i;
         }
-        setCompleted(true)
-        setTodos(arr)
-    }
+        setCompleted(true);
+        setTodos(arr);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    };
 
     useEffect(() => {
-        //maybe set this up to feed from database
-        setTodos(initialTodos);
+        //maybe set this up to feed from database <--- lol yeah right
+        let checkStorage = localStorage.getItem("todos");
+        if (checkStorage.length) {
+            setTodos(JSON.parse(localStorage.getItem("todos")));
+        } else {
+            setTodos(initialTodos);
+        }
     }, []);
 
     useEffect(() => {
-        if(completed){
-            setCompleted(false)
+        if (completed) {
+            setCompleted(false);
         }
     }, [completed]);
 
@@ -78,8 +88,10 @@ function Todo() {
                 {/* <h3 className="lead fs-1 mb-0 pb-0">Add Todo</h3> */}
                 <form className="row ms-0 mb-4" onSubmit={addTodo}>
                     <div className="col-11">
-                        <label for="todo-input" class="form-label fs-2">Add Todo</label>
-                        <input value={value} onChange={handleChange} type="text" class="form-control" id="todo-input" />
+                        <label htmlFor="todo-input" className="form-label fs-2">
+                            Add Todo
+                        </label>
+                        <input value={value} onChange={handleChange} type="text" className="form-control" id="todo-input" />
                     </div>
                     <div className="col-1 d-flex align-items-end">
                         <button onClick={addTodo} className="btn btn-outline-light mt-2" type="button">
@@ -91,10 +103,24 @@ function Todo() {
                     {todos.map((todo) => (
                         <div key={todo.id} className="row my-2">
                             <div className="col-11">
-                                <p style={todo.completed ? {textDecoration: "line-through"} : {}} key={todo.task} className="lead fs-4">-{todo.task}</p>
+                                <p
+                                    style={todo.completed ? { textDecoration: "line-through" } : {}}
+                                    key={todo.task}
+                                    className="lead fs-4"
+                                >
+                                    -{todo.task}
+                                </p>
                             </div>
                             <div className="col-1">
-                                {todo.completed ? <button id={todo.id} onClick={deleteTodo} className="btn btn-outline-danger">Delete</button> : <button id={todo.id} onClick={completeTodo} className="btn btn-outline-success">Done</button>}
+                                {todo.completed ? (
+                                    <button id={todo.id} onClick={deleteTodo} className="btn btn-outline-danger">
+                                        Delete
+                                    </button>
+                                ) : (
+                                    <button id={todo.id} onClick={completeTodo} className="btn btn-outline-success">
+                                        Done
+                                    </button>
+                                )}
                             </div>
                         </div>
                     ))}
