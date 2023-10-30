8e47b87e8fe757c472ac73ce0de3c63fa15be8bf
Griffin Scott <griffinoscott@gmail.com>
2023-10-30 04:49:39 -0700

    started building functionality for retrieving token for spotify api

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,9 +1,38 @@
 // import './HomePage.css';
 
+import { useEffect, useState } from "react";
+
 function HomePage() {
+    const [token, setToken] = useState("");
+
+    const getToken = async () => {
+        /*
+            Need to look at Auth section in
+            Postman to see whats going on
+            and how to turn into a function
+        */
+
+
+        setToken('token123')
+        window.localStorage.setItem('token', token)
+    };
+
+    useEffect(() => {
+        setToken(window.localStorage.getItem("token"));
+    }, []);
+
     return (
-        <div className="HomePage">
-            <h1 className="display-1 text-center">Home Page</h1>
+        <div className="HomePage container">
+            <div className="col-5 mx-auto d-flex flex-column justify-content-center">
+                <h1 className="display-1 text-center">Home Page</h1>
+                {token ? (
+                    <p className="lead text-center">Token active!</p>
+                ) : (
+                    <button onClick={getToken} className="btn btn-lg btn-success mx-5">
+                        Get Token
+                    </button>
+                )}
+            </div>
         </div>
     );
 }
--- client/src/Layout.js
+++ client/src/Layout.js
@@ -4,8 +4,8 @@ import "./layout.css";
 const Layout = () => {
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-black py-0 mb-1">
-                <div class="container-fluid justify-content-between d-flex flex-row my-0">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-black py-0 mb-1">
+                <div className="container-fluid justify-content-between d-flex flex-row my-0">
                     <div className="col-4">
                         <Link className="nav-link active navbar-brand fs-2 me-0" to="/">
                             <img id="cat-logo" src="catIcon.png" alt="" />
@@ -13,27 +13,27 @@ const Layout = () => {
                         </Link>
                     </div>
                     <div className="col-4">
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-primary" type="submit">
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-primary" type="submit">
                                 Search
                             </button>
                         </form>
 
                     </div>
                     <div className="col-4 d-flex justify-content-end align-items-end text-center">
-                        <ul class="navbar-nav d-flex align-items-center">
-                            <li class="nav-item nb-link me-1">
+                        <ul className="navbar-nav d-flex align-items-center">
+                            <li className="nav-item nb-link me-1">
                                 <Link className="nav-link active lead" to="/discover">
                                     Discover
                                 </Link>
                             </li>
-                            <li class="nav-item nb-link">
+                            <li className="nav-item nb-link">
                                 <Link className="nav-link active lead" to="/todo">
                                     Todos
                                 </Link>
                             </li>
-                            <li class="nav-item nb-link ms-1">
+                            <li className="nav-item nb-link ms-1">
                                 <Link className="nav-link active lead" to="/account">
                                     Account
                                 </Link>
